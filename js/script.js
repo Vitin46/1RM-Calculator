69d5e5cca950a0fc9339c72e4313da923e9b175b
vitin46 <viegidulira@gmail.com>
2024-05-10 10:58:24 -0300

    estiliação, e inicio do javaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 //formula:            1RM = Weight (1 + Reps/30)
+
+const calculaRm = (input1, input2) => {
+    carga = input1.value;
+    reps = input2.value;   
+    
+    
+    let rm = input1 * (1 + (input2/30));
+}
+
